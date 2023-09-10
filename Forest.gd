cb6358b0a462610ae5921395537585db3331e464
Benjamin McMahon <parachutehappens@gmail.com>
2023-09-10 11:25:41 -0700

    cave entrance and basic tilemap built

--- Forest.gd
+++ Forest.gd
@@ -9,6 +9,8 @@ func _ready():
 	var x = $CollisionShape2D.position.x * 2
 	var y = $CollisionShape2D.position.y * 2
 	loot_generator.generate(self, player_ref, Vector2(x, y))
+	
+	$ScenePortal.set_scene_path("res://Regions/Cave/cave.tscn")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
