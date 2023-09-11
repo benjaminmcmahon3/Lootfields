091a079e55dc9dbb3293dd06de2bf41eea221e11
Mason Zeimet <masonzeimet@Masons-MBP.home>
2023-09-10 20:42:02 -0500

    Change Food.tscn to LootItem.tscn

--- scripts/LootGenerate.gd
+++ scripts/LootGenerate.gd
@@ -1,10 +1,10 @@
 const json_loader = preload("res://scripts/JsonLoader.gd")
 
-static var food_scene: PackedScene = preload("res://Food.tscn")
+static var food_scene: PackedScene = preload("res://LootItem.tscn")
 
 static func generate(parent_node: Node, player_node, coords: Vector2, loot_file_path):
 	var loot_config: Dictionary = json_loader._loadJson(loot_file_path)
-	
+
 	for loot_key in loot_config.keys():
 		var loot_item = loot_config[loot_key]
 		var item_quantity = loot_item.count
@@ -13,7 +13,7 @@ static func generate(parent_node: Node, player_node, coords: Vector2, loot_file_
 			var loot_instance = food_scene.instantiate()
 			loot_instance.set_item_name(loot_item.name)
 			loot_instance.set_texture(loot_item.path)
-			
+
 			var new_x = randi() % int(coords.x)
 			var new_y = randi() % int(coords.y)
 			loot_instance.position = Vector2(new_x, new_y)
