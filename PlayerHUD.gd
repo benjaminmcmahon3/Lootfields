be03eb527d16af7a748a0a6b2e265269eba88e7a
Mason Zeimet <masonzeimet@Masons-MBP.home>
2023-09-17 09:34:06 -0500

    Change HUD to update on changes to stats, not in the _process fn

--- PlayerHUD.gd
+++ PlayerHUD.gd
@@ -1,8 +1,13 @@
 extends CanvasLayer
 
-@export var stats: Stats
+@export var stats: Stats = null:
+	set = set_stats
 
 @onready var ui_health := $Health
 
-func _process(delta):
+func set_stats(new_stats: Stats):
+	stats = new_stats
+	stats.connect("changed", Callable(self, "update_stats_hud"))
+
+func update_stats_hud():
 	ui_health.value = stats.health
