a1b4be798b32f0b9859f34f3596a3226cb13f910
Mason Zeimet <masonzeimet@Masons-MBP.home>
2023-09-20 23:06:21 -0500

    Use tab container for inventory

--- Interface/Elements/InventoryUI.gd
+++ Interface/Elements/InventoryUI.gd
@@ -5,7 +5,7 @@ const inventory_item: PackedScene = preload("res://Interface/Elements/InventoryL
 @export var inventory: Inventory = null:
 	set = set_inventory
 
-@onready var inventory_list := $InventoryList
+@onready var inventory_tab_container := $PrototypeInventory
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -21,15 +21,20 @@ func set_inventory(new_inventory: Inventory) -> void:
 	inventory.connect("changed", Callable(self, "update_items_display"))
 
 func update_items_display():
-	for n in inventory_list.get_children():
-		inventory_list.remove_child(n)
-		n.queue_free()
+	for tab in inventory_tab_container.get_children():
+		var tab_children = tab.get_children()
+
+		for n in tab_children:
+			tab.remove_child(n)
+			n.queue_free()
 
 	print("inventory.items : ", inventory.items())
-	for item in inventory.items():
-		var loot_item_instance = preload("res://Interface/Elements/InventoryListItem.tscn").instantiate()
-		var item_data = ItemDb.get_item_data(item.unique_id)
-		loot_item_instance.get_node("ItemName").text = item_data.display_name
-		loot_item_instance.get_node("Quantity").text = str(item.count)
-
-		inventory_list.add_child(loot_item_instance)
+	
+	for tab in inventory_tab_container.get_children():
+		for item in inventory.items():
+			if tab.name == "Food":
+				var loot_item_instance = preload("res://Interface/Elements/InventoryListItem.tscn").instantiate()
+				var item_data = ItemDb.get_item_data(item.unique_id)
+				loot_item_instance.get_node("ItemName").text = item_data.display_name
+				loot_item_instance.get_node("Quantity").text = str(item.count)
+				tab.add_child(loot_item_instance)	
